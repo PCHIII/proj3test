688e2ee4167e919897b44b50a8c96b6449565f33
PCHIII <treyhelmer@gmail.com>
2019-10-24 21:19:48 -0400

    colors

--- src/views/Dashboard/components/UsersByDevice/UsersByDevice.js
+++ src/views/Dashboard/components/UsersByDevice/UsersByDevice.js
@@ -42,67 +42,7 @@ const UsersByDevice = props => {
   const { className, ...rest } = props;
 
   const classes = useStyles();
-  // const theme = useTheme();
-
-  // const data = {
-  //   datasets: [
-  //     {
-  //       data: [63, 15, 22],
-  //       backgroundColor: [
-  //         theme.palette.primary.main,
-  //         theme.palette.error.main,
-  //         theme.palette.warning.main
-  //       ],
-  //       borderWidth: 8,
-  //       borderColor: theme.palette.white,
-  //       hoverBorderColor: theme.palette.white
-  //     }
-  //   ],
-  //   labels: ['Desktop', 'Tablet', 'Mobile']
-  // };
-
-  // const options = {
-  //   legend: {
-  //     display: false
-  //   },
-  //   responsive: true,
-  //   maintainAspectRatio: false,
-  //   animation: false,
-  //   cutoutPercentage: 80,
-  //   layout: { padding: 0 },
-  //   tooltips: {
-  //     enabled: true,
-  //     mode: 'index',
-  //     intersect: false,
-  //     borderWidth: 1,
-  //     borderColor: theme.palette.divider,
-  //     backgroundColor: theme.palette.white,
-  //     titleFontColor: theme.palette.text.primary,
-  //     bodyFontColor: theme.palette.text.secondary,
-  //     footerFontColor: theme.palette.text.secondary
-  //   }
-  // };
-
-  // const devices = [
-  //   {
-  //     title: 'Desktop',
-  //     value: '63',
-  //     icon: <LaptopMacIcon />,
-  //     color: theme.palette.primary.main
-  //   },
-  //   {
-  //     title: 'Tablet',
-  //     value: '15',
-  //     icon: <TabletMacIcon />,
-  //     color: theme.palette.error.main
-  //   },
-  //   {
-  //     title: 'Mobile',
-  //     value: '23',
-  //     icon: <PhoneIphoneIcon />,
-  //     color: theme.palette.warning.main
-  //   }
-  // ];
+ 
 
   return (
     <Card
@@ -115,7 +55,7 @@ const UsersByDevice = props => {
             <RefreshIcon />
           </IconButton>
         }
-        title="Users By Device"
+        title="Users in Chat"
       />
       <Divider />
       <CardContent>
